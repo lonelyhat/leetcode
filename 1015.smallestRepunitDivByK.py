e95235705787aa17dbe3d5fa9321a2672b401dd5
lonelyhat <avalonglory@gmail.com>
2023-05-10 20:18:05 +0800

    5/10

--- 1015.smallestRepunitDivByK.py
+++ 1015.smallestRepunitDivByK.py
@@ -1,7 +1,9 @@
 class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
+        if (k%2==0 or k%5 ==0):
+            return -1
         n = 0
-        for i in range(k+1):
+        for i in range(k):
             n = n*10 +1
             if (n%k)==0:
                 return i+1
